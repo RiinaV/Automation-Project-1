68e6f563725255408fbf317e9f922f136476cce7
RiinaV <riinave8@gmail.com>
2023-11-30 21:29:50 +0200

    My commets about the commit

--- cypress/e2e/registration_form_3_test.cy.js
+++ cypress/e2e/registration_form_3_test.cy.js
@@ -13,6 +13,74 @@ Task list:
     * email format
  */
 
+describe('Visual test for registration form 3', () => {
+    it('Check that logo is correct and has correct size', () => {
+        cy.log('Will check logo source and size')
+        cy.get('img').should('have.attr', 'src').should('include', 'cerebrum_hub_logo')
+        // get element and check its parameter height, to less than 178 and greater than 100
+        cy.get('img').invoke('height').should('be.lessThan', 178)
+            .and('be.greaterThan', 100)      
+                     
+    })
+
+    it('Check that country dropdown is correct', () => {
+        // Array of found elements with given selector has 4 elements in total
+        cy.get('#country').children().should('have.length', 4)
+
+        //Verify values in the country dropdown
+        cy.get('#country').find('option').eq(0).should('have.text', '')
+        cy.get('#country').find('option').eq(1).should('have.text', 'Spain')
+        cy.get('#country').find('option').eq(2).should('have.text', 'Estonia')
+        cy.get('#country').find('option').eq(3).should('have.text', 'Austria')     
+               
+    });
+
+    it.only('Check that cities list is correct', () => {
+        //Select a country from a dropdown
+        cy.get('#country').select('Austria')
+
+        // Array of found elements in the city dropdown has 4 elements in total
+        cy.get('#city').children().should('have.length', 4)
+
+        //Check the displayed values in the city dropdown
+        cy.get('#city').find('option').eq(0).should('have.text', '')
+        cy.get('#city').find('option').eq(1).should('have.text', 'Vienna')
+        cy.get('#city').find('option').eq(2).should('have.text', 'Salzburg')
+        cy.get('#city').find('option').eq(3).should('have.text', 'Innsbruck')
+
+        
+
+
+        
+    });
+
+    it('Check that radio button list is correct', () => {
+        // Array of found elements with given selector has 4 elements in total
+        cy.get('input[type="radio"]').should('have.length', 4)
+
+        // Verify labels of the radio buttons
+        cy.get('input[type="radio"]').next().eq(0).should('have.text','Daily')
+        cy.get('input[type="radio"]').next().eq(1).should('have.text','Weekly')
+        cy.get('input[type="radio"]').next().eq(2).should('have.text','Monthly')
+        cy.get('input[type="radio"]').next().eq(3).should('have.text','Never')
+
+         //Verify default state of radio buttons
+        cy.get('input[type="radio"]').eq(0).should('not.be.checked')
+        cy.get('input[type="radio"]').eq(1).should('not.be.checked')
+        cy.get('input[type="radio"]').eq(2).should('not.be.checked')
+        cy.get('input[type="radio"]').eq(3).should('not.be.checked')
+
+         // Selecting one will remove selection from other radio button
+         cy.get('input[type="radio"]').eq(0).check().should('be.checked')
+         cy.get('input[type="radio"]').eq(1).check().should('be.checked')
+         cy.get('input[type="radio"]').eq(0).should('not.be.checked')
+
+    });
+
+
+    
+});
+        
 
 /*
 BONUS TASK: add functional tests for registration form 3
